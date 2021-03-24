8a662a05d29ab25cc6bc67f82f073cb33c0224c2
Raven <rhold.navarette@kodakollectiv.com>
2021-03-24 10:53:55 +0800

    Final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
+        format.js
         format.html { redirect_to @post, notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
